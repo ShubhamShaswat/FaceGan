06b228ff83f9a8feebee46a227a0637a66a5b009
ShubhamShaswat <shaswat.cpp@gmail.com>
2021-07-20 23:40:27 +0530

    genrator loss test

--- loss/loss.py
+++ loss/loss.py
@@ -28,3 +28,42 @@ class GeneratorLoss(object):
 
     def __call__(self,Dx_hat):
         return tf.reduce_mean(-Dx_hat)
+        
+ 
+ 
+ 
+
+##Test Loss---------------------------------------------------------------------------------------
+gen_loss = GeneratorLoss()
+critic_loss = CriticLoss()
+
+# UNIT TEST
+tf.debugging.assert_near(
+    gen_loss(tf.constant(1.)),
+    tf.constant(-1.0)
+)
+print("Success!")
+
+
+
+def gradient_penalty(x_interpolated):
+	gradients = tf.gradients(discriminator(x_interpolated, training=True), [x_interpolated,])[0]
+	grad_l2 = tf.sqrt(tf.reduce_sum(tf.square(gradients), axis=[1, 2, 3]))
+	grad_penalty = tf.reduce_mean(tf.square(grad_l2 - 1.0))
+	return grad_penalty 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
